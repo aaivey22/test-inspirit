8ad51385a6495399362942085d65fbc37957b855
aaivey22 <angelawings22@gmail.com>
2020-07-10 17:19:48 -0400

    added product list in dataJsn file, productsJS file, and images folder.

--- app/src/App.js
+++ app/src/App.js
@@ -1,19 +1,34 @@
 import React from 'react';
+import data from "./data.json";
+import { render } from 'react-dom';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">INSPIRIT</a>
-      </header>
-      <main>
-        Product List
+//class component
+class App extends React.Component {
+  constructor(){
+    super();
+    this.state = {
+      products: data.products,
+      options: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <div className="grid-container">
+        <header>
+          <a href="/">INSPIRIT</a>
+        </header>
+        <main>
+          <div className="content">
+            <div className="main">Products</div>
+            <div className="sidebar">Cart</div>
+          </div>
       </main>
-      <footer>
-        All Rights Reserved
+        <footer>
+          All Rights Reserved
       </footer>
-    </div>
-  );
+      </div>
+    );
+  }
 }
-
 export default App;

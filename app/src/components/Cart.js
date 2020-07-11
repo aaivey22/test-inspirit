c91d3d98aa209ac8b35ca377edb5508370e841f7
aaivey22 <angelawings22@gmail.com>
2020-07-11 00:56:30 -0400

    added createOrder form and set the default state to false with a handleinput to update the state with the user info

--- app/src/components/Cart.js
+++ app/src/components/Cart.js
@@ -9,7 +9,34 @@ import formatCurrency from '../utils';
     // and the quotient of the current item price by the current item count. Default value is set to 0.
     //conditional rendering is used to hide/display the proceed button
 
+// Constructor(props) by default will not show the checkout form unless the proceed button is clicked
+// handleInput func updates the component state by accessing the input box name and it's value
+
 export default class Cart extends Component {
+    constructor(props){
+        super(props);
+        this.state = { 
+            name:"",
+            email:"",
+            address:"",
+            showCheckout: false }
+    }
+
+    handleInput = (e) => {
+        this.setState({ [e.target.name]: e.target.value })
+    }
+
+    createOrder = (e) => {
+        e.preventDefault();
+        const order = {
+            name: this.state.name,
+            email: this.state.email,
+            address: this.state.address,
+            cartItems: this.props.cartItems,
+        };
+        this.props.createOrder(order);
+    };
+
     render() {
 
         const { cartItems } = this.props;
@@ -39,6 +66,7 @@ export default class Cart extends Component {
                     </ul>
                 </div>
                 {cartItems.length!==0 && (
+                    <div>
                     <div className="cart">
                         <div className="total">
                             <div>
@@ -47,8 +75,36 @@ export default class Cart extends Component {
                                     cartItems.reduce((a, c) => a + c.price * c.count, 0)
                                 )}
                             </div>
-                            <button className="button primary">Proceed</button>
+                            <button onClick={() => {
+                                this.setState({showCheckout: true});
+                                }}className="button primary">Proceed</button>
                         </div>
+                    </div>
+                    {this.state.showCheckout && (
+                      <div className="cart">
+                        <form onSubmit={this.createOrder}>
+                            <ul className="form-container">
+                                <li>
+                                    <label>Email</label>
+                                    <input name="email" type="email" required onChange={this.handleInput}></input>
+                                </li>
+                                <li>
+                                    <label>Name</label>
+                                    <input name="name" type="text" required onChange={this.handleInput}></input>
+                                </li>
+                                <li>
+                                    <label>Address</label>
+                                    <input name="address" type="text" required onChange={this.handleInput}></input>
+                                </li>
+                                <li>
+                                    <button className="button primary" type="submit">
+                                        Checkout
+                                    </button> 
+                                </li>
+                            </ul>
+                        </form>
+                        </div>
+                    )}
                     </div>
                 )}
             </div>
